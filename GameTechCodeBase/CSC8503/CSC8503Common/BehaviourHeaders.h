bb29a29fd8ca073174bd4fd736c26968538158c8
ben-lendrem <33862184+ben-lendrem@users.noreply.github.com>
2022-04-11 13:53:02 +0100

    Delete GameTechCodeBase directory
    I'm pretty sure this is an old version, so I have deleted it to replace it with the proper version

--- GameTechCodeBase/CSC8503/CSC8503Common/BehaviourHeaders.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "../CSC8503Common/BehaviourAction.h"
-#include "../CSC8503Common/BehaviourNode.h"
-#include "../CSC8503Common/BehaviourNodeWithChildren.h"
-#include "../CSC8503Common/BehaviourSelector.h"
-#include "../CSC8503Common/BehaviourSequence.h"
